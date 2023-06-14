8831e22b6dfe272674a73f790f098034739d87bf
mohammadrezaghelichkhani <ghelichkhaniohammadreza55@gmail.com>
2023-06-14 04:57:56 -0700

    Log in and Jwt (log in="Ali Kazempour",Jwt="Mohammad Reza GHelichKhani")

---
+++ FantasyFootballProject/FantasyFootballProject/Business/Token.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FantasyFootballProject.DataBase;
+using System.Text;
+
+
+namespace FantasyFootballProject.Business;
+
+public class Token
+{
+    public static Object LogIn(string password, string username)
+    {
+        using (var db = new Database())
+        {
+            foreach (var members in db.Users)
+            {
+                if (members.Username == username && members.Password == password)
+                {
+                    return generateToken(username);
+                    /*return "Welcome to FootballFantasi\n";//+//token;*/
+                }
+            }
+            return "Your username or password is wrong!!!";
+        }
+    }
+    public static string generateToken(string userName)
+    {
+        var securityKey =
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Your Friendly Neighborhood Spiderman"));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        var claims = new[]
+        {
+            new Claim("UserName", userName),
+        };
+        var token = new JwtSecurityToken(
+            issuer: "http://localhost:5231",
+            audience: "http://localhost:5231",
+            claims,
+            expires: DateTime.Now.AddSeconds(1),
+            signingCredentials: credentials
+        );
+        var stringToken = new JwtSecurityTokenHandler().WriteToken(token);
+        return stringToken;
+
+    }
+    public static string decodeToken(string Token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var jsonToken = handler.ReadJwtToken(Token);
+        var x = jsonToken.Claims.First(claim => claim.Type == "UserName").Value;
+        return x;
+    }
+}
