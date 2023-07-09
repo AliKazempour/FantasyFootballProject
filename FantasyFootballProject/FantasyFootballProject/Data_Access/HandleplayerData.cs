596eb11fa35d7e64f4af16831dd25f335d8b8ffd
AliKazempour <Alikazempoor83@gmail.com>
2023-07-09 20:00:03 +0330

    Having Problem!!!

--- FantasyFootballProject/FantasyFootballProject/Data_Access/HandleplayerData.cs
+++ FantasyFootballProject/FantasyFootballProject/Data_Access/HandleplayerData.cs
@@ -21,5 +21,36 @@ namespace FantasyFootballProject.Data_Access
                 db.SaveChanges();
             }
         }
+
+        public static List<Player> SearchPlayers(string searchTerm)  //--> Ghelich
+        {
+            var db = new Database();
+            var filteredPlayers = db.players
+                .Where(p => p.first_name.Contains(searchTerm) || p.second_name.Contains(searchTerm)).ToList();
+            return filteredPlayers;
+        }
+
+        public static List<Player> SortPlayersByPrice(bool ascending)   //--> Ghelich
+        {
+            var db = new Database();
+            var sortedPlayers = ascending
+                ? db.players.OrderBy(p => p.now_cost).ToList()
+                : db.players.OrderByDescending(p => p.now_cost).ToList();
+            return sortedPlayers;
+        }
+
+        public static List<Player> FilterPlayersByPosition(string position)   //-->Ghelich
+        {
+            var db = new Database();
+            var filteredPlayers = db.players.Where(p => p.element_type.Equals(position)).ToList();
+            return filteredPlayers;
+        }
+
+        public static List<Player> ShowPlayers(int page, int pageSize)  //-->Nourozi
+        {
+            var db = new Database();
+            var pagedPlayers = db.players.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return pagedPlayers;
+        }
     }
 }
--- FantasyFootballProject/FantasyFootballProject/Presention/Api.cs
+++ FantasyFootballProject/FantasyFootballProject/Presention/Api.cs
@@ -16,7 +16,11 @@ namespace FantasyFootballProject.Presention
             app.MapPost("/user/veritification/", memberLogic.Verification);
             app.MapGet("/user/login/", Token.LogIn);
             app.MapPost("/user/addPlayer/", logic.AddPlayer);
-
+            
+            app.MapGet("user/searchPlayer/", HandleplayerData.SearchPlayers);
+            app.MapGet("user/SortPlayers/", HandleplayerData.SortPlayersByPrice);
+            app.MapGet("user/FilterPlayers/", HandleplayerData.FilterPlayersByPosition);
+            app.MapGet("user/ShowPlayers/", HandleplayerData.ShowPlayers);
             app.Run("http://localhost:7171");
         }
     }
