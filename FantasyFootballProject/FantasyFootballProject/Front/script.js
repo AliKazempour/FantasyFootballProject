9befcb1c85c920796db3e135396dce1371562598
mohammadrezaghelichkhani <ghelichkhaniohammadreza55@gmail.com>
2023-07-23 01:31:03 -0700

    js login

--- FantasyFootballProject/FantasyFootballProject/Front/script.js
+++ FantasyFootballProject/FantasyFootballProject/Front/script.js
@@ -12,3 +12,19 @@ signupLink.addEventListener('click', function () {
     signupForm.classList.remove('hidden');
     loginForm.classList.add('hidden');
 });
+
+var login = document.getElementById('Login');
+login.addEventListener('click', function (e) {
+    e.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    var url = `http://localhost:7096/user/login/?password=${password}&username=${username}`;
+    fetch(url).then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            alert(data.error);
+
+        })
+});
