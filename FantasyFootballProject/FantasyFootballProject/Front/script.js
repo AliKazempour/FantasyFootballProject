a9c486ec47648e8b10ab185949724b5c88a82c42
Sepehr Noroozi <noroozi.sepehr@gmail.com>
2023-07-23 09:24:02 +0330

    Linking Sign-Up and Log-In form

---
+++ FantasyFootballProject/FantasyFootballProject/Front/script.js
@@ -0,0 +1,14 @@
+﻿const loginLink = document.querySelector('.login-link');
+const signupLink = document.querySelector('.signup-link');
+const loginForm = document.querySelectorAll('.form-container')[0];
+const signupForm = document.querySelectorAll('.form-container')[1];
+
+loginLink.addEventListener('click', function () {
+    loginForm.classList.remove('hidden');
+    signupForm.classList.add('hidden');
+});
+
+signupLink.addEventListener('click', function () {
+    signupForm.classList.remove('hidden');
+    loginForm.classList.add('hidden');
+});
